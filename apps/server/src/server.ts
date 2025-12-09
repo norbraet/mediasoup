38c5b5741a20dfcb0a9f5ab0fb41592d86d13119
Norbert Balaž <norbusmaximus@gmail.com>
2025-12-09 13:51:21 +0100

    feat(server): setup socketio

--- apps/server/src/server.ts
+++ apps/server/src/server.ts
@@ -1,10 +1,27 @@
-import { createServer } from 'http'
+import fs from 'fs'
+import { createServer } from 'https'
 import { createApp } from './app'
 import env from './config/env'
+import { Server as SocketIOServer } from 'socket.io'
 
-async function main() {
+async function main(): Promise<void> {
   const app = createApp()
-  const httpServer = createServer(app).listen(env.EXPRESS_PORT)
+
+  // Keys were made with mkcert
+  const key = fs.readFileSync('./config/create-cert-key.pem')
+  const cert = fs.readFileSync('./config/create-cert.pem')
+  const options = {
+    key: key,
+    cert: cert,
+  }
+
+  const httpsServer = createServer(options, app).listen(env.EXPRESS_PORT)
+  new SocketIOServer(httpsServer, {
+    cors: {
+      origin: env.CORS_ORIGIN,
+      methods: ['GET', 'POST'],
+    },
+  })
 
   console.log('Server is running on port:', env.EXPRESS_PORT)
 }
