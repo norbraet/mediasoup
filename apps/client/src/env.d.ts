84a93b754ae2bb9ec88dcb7d4be6c94157775912
Norbert Balaž <norbusmaximus@gmail.com>
2025-12-13 00:43:44 +0100

    fix(Frontend): fixes eslint error for generic Vue Component declaration

--- apps/client/src/env.d.ts
+++ apps/client/src/env.d.ts
@@ -3,6 +3,7 @@
 import type { DefineComponent } from 'vue'
 
 declare module '*.vue' {
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
   const component: DefineComponent<{}, {}, any>
   export default component
 }
